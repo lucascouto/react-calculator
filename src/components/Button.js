f8c6890ad50c8aa59f00985c4e8224ca13cf1534
Lucas Couto <loc.unb@gmai.com>
2022-08-22 00:16:12 -0300

    Finalize button style and adds click event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,19 @@
+import "./Button.css";
+
 const Button = (props) => {
-  return <button className="button">{props.label}</button>;
+  return (
+    <button
+      onClick={() => props.click && props.click(props.label)}
+      className={`
+        button 
+        ${props.operation ? "operation" : ""} 
+        ${props.double ? "double" : ""} 
+        ${props.triple ? "triple" : ""}
+    `}
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
