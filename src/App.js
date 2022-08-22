0e6f3f84573b7773e1d3892cbf451a64ff67b094
Lucas Couto <loc.unb@gmai.com>
2022-08-21 21:48:21 -0300

    Creates calculator component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Calculator from "./main/Calculator.js";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </>
+  );
 }
 
 export default App;
